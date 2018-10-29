e3f47ff627a4004f71e642564b9fc41bb264cdf1
mjimenez1 <mjimenez@anzen.com.mx>
2018-10-28 18:22:56 -0600

    cambios pantalla premiame

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ function changeView(nameView) {
     switch (nameView) {
         case 'premiame.html':
             $("#wrapper-section").load("views/" + nameView)
+            if($("#menu-general").is(":visible")){
+            	$("#menu-general").trigger("click")
+            }
             break;
     }
 }
